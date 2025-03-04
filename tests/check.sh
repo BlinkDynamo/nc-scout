cb186ac701ae8f24eefb698f81276eebffba7ea2
Josh Hayden <blindcollective@protonmail.com>
2025-03-03 20:20:58 -0500

    Changed check_search() to take a simple command as input. check_search() now has a third output, error, that is displayed when the test_command being ran exits abnormally.

--- tests/check.sh
+++ tests/check.sh
@@ -24,53 +24,54 @@ tests_executed=0
 
 function check_search()
 {
-    local test_name="$1"            # The name of the test itself, as well as the directory the test is testing.
-    local convention="$2"           # The naming convention being tested.
-    local options="$3"              # Any options being passed to nc-scout search.
-    local n_expected_correct="$4"   # The expected number of lines of output from the command.
-
     (( tests_executed++ ))
-
-    # Search inside the test directory of a naming convention, ignoring the name of the test directory itself.
-    local n_observed_correct=$( \
-        ${BUILD_DIR}/nc-scout search ${options} ${convention} ${TESTS_DIR}/${test_name} | wc -l )
     
-    if [ "$n_observed_correct" -eq "$n_expected_correct" ]; then
-        printf "[${GREEN}✓${RESET}]"
-        (( tests_passed++ ))
+    local test_command="$1"         # The command to execute, capturing it's lines of output.
+    local n_expected_correct="$2"   # The expected number of lines of output from the command.
+     
+    # Firstly, check if the test_command exits with a non-zero exit code. If so, print an error message.
+    if ! $test_command>/dev/null; then
+        printf "%b %s\n" "[${RED}!${RESET}]" "Error: Test '$test_name' exited abnormally."
     else
-        printf "[${RED}X${RESET}]"  
-    fi
-    # Make the output look pretty and aligned.
-    printf " %-40s | %s | %s\n" \
-           "${TESTS_DIR}/${test_name}" \
-           "matches expected: $n_expected_correct" \
-           "matches observed: $n_observed_correct" 
+        local n_observed_correct=$($test_command | wc -l)
+        
+        if [ "$n_observed_correct" -eq "$n_expected_correct" ]; then
+            printf "%b %s\n" \
+                "[${GREEN}✓${RESET}]" \
+                "Pass: $test_command"
+            (( tests_passed++ ))
+        else
+            printf "%b %s -> %s\n" \
+                "[${RED}X${RESET}]" \
+                "Fail: $test_command" \
+                "Matches observed: $n_observed_correct"
+        fi
+    fi     
 }
 
 #----------------------------------------------------------------------------------------------#
 # Test Execution
 #----------------------------------------------------------------------------------------------#
-printf "\nTesting 'search' subcommand.\n\n"
+printf "\nTesting 'search' subcommand. Expecting 10 matches for each test.\n\n"
 
 printf "${ORANGE}Tests for strict matches:${RESET}\n"
-check_search "flatcase_strict_matches" "flatcase" "-sm" 10
-check_search "camelcase_strict_matches" "camelcase" "-sm" 10
-check_search "pascalcase_strict_matches" "pascalcase" "-sm" 10
-check_search "snakecase_strict_matches" "snakecase" "-sm" 10
-check_search "constantcase_strict_matches" "constantcase" "-sm" 10
-check_search "kebabcase_strict_matches" "kebabcase" "-sm" 10
-check_search "cobolcase_strict_matches" "cobolcase" "-sm" 10
+check_search "./$BUILD_DIR/nc-scout search -sm flatcase $TESTS_DIR/flatcase_strict_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -sm camelcase $TESTS_DIR/camelcase_strict_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -sm pascalcase $TESTS_DIR/pascalcase_strict_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -sm snakecase $TESTS_DIR/snakecase_strict_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -sm constantcase $TESTS_DIR/constantcase_strict_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -sm kebabcase $TESTS_DIR/kebabcase_strict_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -sm cobolcase $TESTS_DIR/cobolcase_strict_matches" 10
 printf "\n"
 
 printf "${ORANGE}Tests for lenient matches:${RESET}\n"
-check_search "flatcase_lenient_matches" "flatcase" "-m" 10
-check_search "camelcase_lenient_matches" "camelcase" "-m" 10
-check_search "pascalcase_lenient_matches" "pascalcase" "-m" 10
-check_search "snakecase_lenient_matches" "snakecase" "-m" 10
-check_search "constantcase_lenient_matches" "constantcase" "-m" 10
-check_search "kebabcase_lenient_matches" "kebabcase" "-m" 10
-check_search "cobolcase_lenient_matches" "cobolcase" "-m" 10
+check_search "./$BUILD_DIR/nc-scout search -m flatcase $TESTS_DIR/flatcase_lenient_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -m camelcase $TESTS_DIR/camelcase_lenient_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -m pascalcase $TESTS_DIR/pascalcase_lenient_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -m snakecase $TESTS_DIR/snakecase_lenient_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -m constantcase $TESTS_DIR/constantcase_lenient_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -m kebabcase $TESTS_DIR/kebabcase_lenient_matches" 10
+check_search "./$BUILD_DIR/nc-scout search -m cobolcase $TESTS_DIR/cobolcase_lenient_matches" 10
 printf "\n"
 
 printf "\nTesting completed. %s/%s tests were successful.\n\n" "$tests_passed" "$tests_executed"
